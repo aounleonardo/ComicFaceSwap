1f9fee26cdd2d4063add09c1e227ea2b490a3fad
Leonardo Aoun <Leonardo Aoun>
2017-04-05 16:26:56 -0700

    add CustomAdapter and navigation between MainActivity and UniverseActivity

--- app/src/main/java/com/example/aounl/comicfaceswap/UniversesActivity.java
+++ app/src/main/java/com/example/aounl/comicfaceswap/UniversesActivity.java
@@ -3,13 +3,23 @@ package com.example.aounl.comicfaceswap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class UniversesActivity extends AppCompatActivity {
+    ListView lv;
+    Model[] modelItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universes);
+        lv = (ListView) findViewById(R.id.listView1);
+        modelItems = new Model[2];
+        modelItems[0] = new Model("heroes", false);
+        modelItems[1] = new Model("villains", true);
+
+        CustomAdapter adapter = new CustomAdapter(this, modelItems);
+        lv.setAdapter(adapter);
     }
 
     public void done(View view){
